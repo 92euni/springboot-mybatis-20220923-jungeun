739023fe04f50b10bb60b65310fa1e1f58a3ca59
92euni <92_sudal@naver.com>
2022-09-29 00:40:14 +0900

    mybatis 프로젝트 초기화

---
+++ src/test/java/com/boot/mybatis20220923jungeun/Mybatis20220923JungeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.mybatis20220923jungeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis20220923JungeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
